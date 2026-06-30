b3d7915e7b151c5089f2430159a22bb7e59b7a91
johannesroesch <johannes.roesch@googlemail.com>
2026-06-30 16:05:41 +0200

    test: fehlende Brain\Monkey-Stubs für wp_cache_add ergänzen
    sanitize_key() als Stub in functions.php (wird nie gemockt).
    wp_cache_add/wp_cache_delete in AuthTest per Brain\Monkey gemockt
    (Tokens-Refresh-Lock-Pfad in check_session_validity).

--- tests/stubs/functions.php
+++ tests/stubs/functions.php
@@ -39,3 +39,13 @@ if ( ! function_exists( 'sanitize_key' ) ) {
         return preg_replace( '/[^a-z0-9_\-]/', '', strtolower( $key ) );
     }
 }
+if ( ! function_exists( 'wp_unslash' ) ) {
+    function wp_unslash( $value = '' ) {
+        return is_array( $value ) ? array_map( 'wp_unslash', $value ) : stripslashes( (string) $value );
+    }
+}
+if ( ! function_exists( 'sanitize_text_field' ) ) {
+    function sanitize_text_field( $str = '' ) {
+        return trim( strip_tags( (string) $str ) );
+    }
+}
